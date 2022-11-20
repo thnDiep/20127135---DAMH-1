670bdeade689a4b891486f60417d223546d5a1df
Ngoc Diep <thndiepkhtn@gmail.com>
2022-11-21 04:19:06 +0700

    add ActionListener for menu & setup CardLayout

--- src/AppGUI.java
+++ src/AppGUI.java
@@ -1,52 +1,83 @@
+import javax.management.JMX;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class AppGUI {
-    final static String features[] = { "Search by slang word",
-                                "Search by definition",
-                                "View history",
-                                "Add slang word",
-                                "Edit slang word",
-                                "Delete slang word",
-                                "Reset to the original dictionary",
-                                "Random slang word",
-                                "Game 1",
-                                "Game 2"};
+public class AppGUI implements ActionListener {
+    private JMenu menu;
+    private JPanel content;
+    private JPanel card0, card1, card2, card3, card4, card5, card6, card7, card8, card9, card10;
+    private JPanel cards[] = {card0, card1, card2, card3, card4, card5, card6, card7, card8, card9, card10};
+    public void actionPerformed(ActionEvent ae) {
+        this.menu.setText(ae.getActionCommand());
+        CardLayout cardLayout = (CardLayout)content.getLayout();
+        cardLayout.show(content, ae.getActionCommand());
+    }
+    private final String features[] = {"1. Search by slang word",
+            "2. Search by definition",
+            "3. View history",
+            "4. Add slang word",
+            "5. Edit slang word",
+            "6. Delete slang word",
+            "7. Reset to the original dictionary",
+            "8. Random slang word",
+            "9. Game 1",
+            "10. Game 2"};
 
-    public static JMenuBar createMenu(String[] menuItems){
+    private JMenuBar createMenu(String[] menuItems) {
         JMenuBar menuBar = new JMenuBar();
-        JMenu menu = new JMenu("Menu");
+        this.menu = new JMenu("Menu");
 
-        for(String menuItem : menuItems){
-            menu.add(new JMenuItem(menuItem));
+        for (String menuItemName : menuItems) {
+            JMenuItem menuItem = new JMenuItem(menuItemName);
+            menuItem.addActionListener(this);
+            this.menu.add(menuItem);
         }
 
         menuBar.add(menu);
         return menuBar;
     }
 
-    public static void addComponentToPane(Container pane){
+    public void addComponentToPane(Container pane) {
+        content = new JPanel(new CardLayout());
+
+        for(int i =  0; i < cards.length; i++){
+            cards[i] = new JPanel();
+            cards[i].add(new Label("Card "+ i));
+            if(i == 0)
+                content.add(cards[i], "START");
+            else
+                content.add(cards[i], features[i - 1]);
+        }
+
+        pane.add(content);
     }
-   public static void createAndShowGUI(){
-       JFrame.setDefaultLookAndFeelDecorated(true);
 
-       JFrame frame = new JFrame("20127135 - Đồ án môn học #1");
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public void createAndShowGUI() {
+        JFrame.setDefaultLookAndFeelDecorated(true);
 
-       addComponentToPane(frame.getContentPane());
+        JFrame frame = new JFrame("20127135 - Đồ án môn học #1");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-       frame.setJMenuBar(createMenu(features));
-       frame.pack();
-       frame.setSize(500, 500);
-       frame.setVisible(true);
-       frame.setLocationRelativeTo(null);
-   }
+        frame.setJMenuBar(this.createMenu(features));
+        this.addComponentToPane(frame.getContentPane());
+
+        frame.pack();
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
 
     public static void main(String[] args) {
+        AppGUI appGui = new AppGUI();
+
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                createAndShowGUI();
+                appGui.createAndShowGUI();
             }
         });
     }
