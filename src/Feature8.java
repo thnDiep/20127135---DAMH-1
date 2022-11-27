2a77d3831c7c468c6c51c1c1e0fc1e825865e088
Ngoc Diep <thndiepkhtn@gmail.com>
2022-11-28 01:12:09 +0700

    Update feature 8 (show/hide definition)

--- src/Feature8.java
+++ src/Feature8.java
@@ -2,22 +2,42 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.Set;
 
-public class Feature8 extends JPanel implements ActionListener {
+public class Feature8 extends JPanel implements ActionListener, ItemListener {
     private final Dictionary dictionary;
-    private final JLabel keyLabel, valueLabel;
+    private final JLabel keyLabel;
+    private final JTextArea valueArea;
+    private final JButton showBtn, randomBtn;
+    private final JCheckBox checkBox;
+    JScrollPane scrollValueArea;
+
+    public void itemStateChanged(ItemEvent e) {
+        if(e.getStateChange() == 1)
+            showDefinition();
+        else
+            hideDefinition();
+    }
 
     public void actionPerformed(ActionEvent ae) {
-        String randomKey = dictionary.randomSlangWord();
-        Set<String> values = dictionary.getDictionary().get(randomKey);
+        if (ae.getSource() == showBtn) {
+            showDefinition();
+        } else if (ae.getSource() == randomBtn) {
+            if(!checkBox.isSelected()){
+                hideDefinition();
+            }
+            String randomKey = dictionary.randomSlangWord();
+            Set<String> values = dictionary.getDictionary().get(randomKey);
 
-        String text = "";
-        for (String value : values) {
-            text += "- " + value + "<br>";
+            valueArea.setText("");
+            for (String value : values) {
+                valueArea.append("- " + value + "\n");
+            }
+
+            keyLabel.setText(randomKey);
         }
-        keyLabel.setText(randomKey.toString());
-        valueLabel.setText("<html>" + text + "</html>");
     }
 
     public Feature8(Dictionary dictionary) {
@@ -25,19 +45,35 @@ public class Feature8 extends JPanel implements ActionListener {
 
         // Label
         keyLabel = new JLabel("");
-        valueLabel = new JLabel("");
         JLabel slangWordLabel = new JLabel("Random slang word: ");
         JLabel definitionLabel = new JLabel("Definitions");
 
         keyLabel.setFont(App.HEADING_FONT);
-        valueLabel.setFont(App.LARGE_FONT);
         slangWordLabel.setFont(App.LARGE_FONT);
-        definitionLabel.setFont(App.HEADING_FONT);
+        definitionLabel.setFont(App.LARGE_FONT);
 
-        // Button
-        JButton button = new JButton("Random");
-        button.setFont(App.LARGE_FONT);
-        button.addActionListener(this);
+        // Text area
+        valueArea = new JTextArea();
+        valueArea.setFont(App.NORMAL_FONT);
+        scrollValueArea = new JScrollPane(valueArea,
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollValueArea.setVisible(false);
+
+        // Buttons
+        randomBtn = new JButton("Random");
+        randomBtn.setFont(App.LARGE_FONT);
+        randomBtn.addActionListener(this);
+
+        showBtn = new JButton("Show definitions");
+        showBtn.setFont(App.LARGE_FONT);
+        showBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+        showBtn.addActionListener(this);
+
+        // Check box
+        checkBox = new JCheckBox("Auto show definitions");
+        checkBox.setFont(App.SMALL_FONT);
+        checkBox.addItemListener(this);
 
         // Header panel
         JPanel headerPanel = new JPanel();
@@ -54,19 +90,13 @@ public class Feature8 extends JPanel implements ActionListener {
         // Center panel
         JPanel centerPanel = new JPanel();
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
-        centerPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-
-        centerPanel.add(Box.createRigidArea(new Dimension(10, 10)));
-        centerPanel.add(valueLabel);
-
-        JScrollPane scrollCenter = new JScrollPane(centerPanel,
-                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        centerPanel.add(showBtn);
+        centerPanel.add(scrollValueArea);
 
         // Line start panel
         JPanel lineStartPanel = new JPanel();
         lineStartPanel.setLayout(new BoxLayout(lineStartPanel, BoxLayout.X_AXIS));
-        lineStartPanel.setPreferredSize(new Dimension(280, 0));
+        lineStartPanel.setPreferredSize(new Dimension(200, 0));
         lineStartPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         lineStartPanel.add(Box.createHorizontalGlue());
@@ -78,19 +108,31 @@ public class Feature8 extends JPanel implements ActionListener {
         bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
         bottomPanel.setPreferredSize(new Dimension(0, 100));
 
+        bottomPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        bottomPanel.add(checkBox);
         bottomPanel.add(Box.createHorizontalGlue());
-        bottomPanel.add(button);
+        bottomPanel.add(randomBtn);
         bottomPanel.add(Box.createHorizontalGlue());
 
         // Container panel
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(headerPanel, BorderLayout.PAGE_START);
         panel.add(lineStartPanel, BorderLayout.LINE_START);
-        panel.add(scrollCenter, BorderLayout.CENTER);
+        panel.add(centerPanel, BorderLayout.CENTER);
         panel.add(bottomPanel, BorderLayout.PAGE_END);
 
         // Card 8
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         add(panel);
+    }
+
+    private void showDefinition(){
+        showBtn.setVisible(false);
+        scrollValueArea.setVisible(true);
+    }
+
+    private void hideDefinition(){
+        showBtn.setVisible(true);
+        scrollValueArea.setVisible(false);
     }
 }
